8225fb691e7e58a1a74a12efd6bcc423477494fe
mmr.K <pollvaulter.m@gmail.com>
2017-09-06 18:05:50 +0900

    add formatting

--- linebot.php
+++ linebot.php
@@ -1,26 +1,25 @@
-
 <?php
- 
+
 $accessToken = 'X20v49IGjBZzfnQ2Ut/pxkysdn47JG+U0665H1LX0s0Y5nN5TLLPs2EZQ0n7BrAPa5Yl5SaxagLOia0+7LEJMA3O22g8tjJ9KbmOnA6wD5/uOtg4YySDFA+oTHOoDHWaLdGLAMRkOb3E5PJ8BQkDFgdB04t89/1O/w1cDnyilFU=';
- 
+
 //ユーザーからのメッセージ取得
 $json_string = file_get_contents('php://input');
 $json_object = json_decode($json_string);
- 
+
 //取得データ
 $replyToken = $json_object->{"events"}[0]->{"replyToken"};        //返信用トークン
 $message_type = $json_object->{"events"}[0]->{"message"}->{"type"};    //メッセージタイプ
-if($message_type == "text") {
+if ($message_type == "text") {
     $message_text = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッセージ内容
 }
 //メッセージタイプが「text」のときは適当な値を返すそれ以外の時は決まった文章を返す
-if($message_type == "text") {
+if ($message_type == "text") {
     //返信メッセージ
     $return_message_text = $message_text . "←とはどういう意味ですか？";
-}elseif($message_type == "sticker"){
+} elseif ($message_type == "sticker") {
     $return_message_text = "そのスタンプかわいいね！";
     $message_type = "text";
-}else{
+} else {
     $return_message_text = "それいいね！？";
     $message_type = "text";
 }
@@ -29,21 +28,24 @@ if($message_type == "text") {
 //返信実行
 sending_messages($accessToken, $replyToken, $message_type, $return_message_text);
 ?>
+
+
 <?php
 //メッセージの送信
-function sending_messages($accessToken, $replyToken, $message_type, $return_message_text){
+function sending_messages($accessToken, $replyToken, $message_type, $return_message_text)
+{
     //レスポンスフォーマット
     $response_format_text = [
         "type" => $message_type,
         "text" => $return_message_text
     ];
- 
+
     //ポストデータ
     $post_data = [
         "replyToken" => $replyToken,
         "messages" => [$response_format_text]
     ];
- 
+
     //curl実行
     $ch = curl_init("https://api.line.me/v2/bot/message/reply");
     curl_setopt($ch, CURLOPT_POST, true);
@@ -57,4 +59,5 @@ function sending_messages($accessToken, $replyToken, $message_type, $return_mess
     $result = curl_exec($ch);
     curl_close($ch);
 }
+
 ?>
